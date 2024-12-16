1ee2b462dca6e7a230257345ca94409e8bdb626b
Lars Schumacher <schumacher90lars@gmail.com>
2024-12-16 14:14:46 +0100

    updatet version

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"simple-crm-e555a","appId":"1:792732140427:web:630f797a1b3abfcbe0e58e","storageBucket":"simple-crm-e555a.firebasestorage.app","apiKey":"AIzaSyBzYqjfvFlz2aAvZksU5ZAjIDcC9fQgchE","authDomain":"simple-crm-e555a.firebaseapp.com","messagingSenderId":"792732140427"})), provideFirestore(() => getFirestore()), provideAnimationsAsync()]
 };
